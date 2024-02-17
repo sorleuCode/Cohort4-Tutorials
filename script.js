0b8276a6f8bfed31cda14960dbb6a4e11fde0f59
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-02-17 15:43:09 +0100

    First Tutorial

---
+++ script.js
@@ -0,0 +1,60 @@
+//  VARIABLE: Global and local scope Variables
+
+// Global Variable 
+
+let i;
+
+for (i = 0; i < 5; i++) {
+    let p = 10;
+    console.log(i + p);
+}
+
+console.log(i);
+
+//  OPERATORS AND EQUALITY
+
+// Arithmetical operators 
+
+//  +, -, *, /, %, ==, ===, increment and decrement
+
+
+//  minus (-):
+
+let cost = 15
+let profit;
+let salesPrice = 20
+
+profit = salesPrice - cost;
+
+console.log(profit);
+
+
+// Plus (+) : for number 
+
+let costs = 15, profits = 5, salesPrices = costs + profit;
+
+console.log(salesPrices);
+
+// plus (+) : for strings
+
+let firstNames = 'Olatunde', surnames = 'Tobi', fullNames = surname + firstName;
+
+console.log(fullName  + " " + "is a big boy");
+
+let firstName = 'Olatunde ', surname = 'Tobi', fullName = surname + firstName;
+
+console.log(`${fullName}  is a big boy`);
+
+// Back tick (``):
+
+let food = 'Rice'
+
+console.log(`What will i eat, if not ${food} ?`);
+
+let A = 2;
+A *= 4;
+
+console.log(A);
+
+
+
