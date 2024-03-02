00254e00203bf55bc40c83d29f5c62b370b26d11
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-03-02 13:48:09 +0100

    object

---
+++ DOM/script.js
@@ -0,0 +1,1 @@
+ document.getElementById("third").innerHTML = "Good";
--- script.js
+++ script.js
@@ -1,144 +1,194 @@
-// //  VARIABLE: Global and local scope Variables
+// // //  VARIABLE: Global and local scope Variables
 
-// // Global Variable 
+// // // Global Variable 
 
-// let i;
+// // let i;
 
-// for (i = 0; i < 5; i++) {
-//     let p = 10;
-//     console.log(i + p);
+// // for (i = 0; i < 5; i++) {
+// //     let p = 10;
+// //     console.log(i + p);
+// // }
+
+// // console.log(i);
+
+// // //  OPERATORS AND EQUALITY
+
+// // // Arithmetical operators 
+
+// // //  +, -, *, /, %, ==, ===, increment and decrement
+
+
+// // //  minus (-):
+
+// // let cost = 15
+// // let profit;
+// // let salesPrice = 20
+
+// // profit = salesPrice - cost;
+
+// // console.log(profit);
+
+
+// // // Plus (+) : for number 
+
+// // let costs = 15, profits = 5, salesPrices = costs + profit;
+
+// // console.log(salesPrices);
+
+// // // plus (+) : for strings
+
+// // let firstNames = 'Olatunde', surnames = 'Tobi', fullNames = surname + firstName;
+
+// // console.log(fullName  + " " + "is a big boy");
+
+// // let firstName = 'Olatunde ', surname = 'Tobi', fullName = surname + firstName;
+
+// // console.log(`${fullName}  is a big boy`);
+
+// // // Back tick (``):
+
+// // let food = 'Rice'
+
+// // console.log(`What will i eat, if not ${food} ?`);
+
+// // let A = 2;
+// // A *= 4;
+
+// // console.log(A);
+
+// // logical operators
+
+// // AND operator:
+
+// let result;
+// let age = 25;
+
+// if (age > 20 && age < 30) {
+//     result = age + 50;
+// }
+// else {
+//     result = age;
 // }
 
-// console.log(i);
+// console.log(result);
 
-// //  OPERATORS AND EQUALITY
+// // OR operator:
 
-// // Arithmetical operators 
+// let cost = 40;
+// let selPrice;
+// let profit = 10;
 
-// //  +, -, *, /, %, ==, ===, increment and decrement
+// if (cost >= 50 || cost === 55) {
+//     selPrice = cost + profit;
+// }
+// else {
+//     selPrice = cost;
+// }
 
-
-// //  minus (-):
-
-// let cost = 15
-// let profit;
-// let salesPrice = 20
-
-// profit = salesPrice - cost;
-
-// console.log(profit);
-
-
-// // Plus (+) : for number 
-
-// let costs = 15, profits = 5, salesPrices = costs + profit;
-
-// console.log(salesPrices);
-
-// // plus (+) : for strings
-
-// let firstNames = 'Olatunde', surnames = 'Tobi', fullNames = surname + firstName;
-
-// console.log(fullName  + " " + "is a big boy");
-
-// let firstName = 'Olatunde ', surname = 'Tobi', fullName = surname + firstName;
-
-// console.log(`${fullName}  is a big boy`);
-
-// // Back tick (``):
-
-// let food = 'Rice'
-
-// console.log(`What will i eat, if not ${food} ?`);
-
-// let A = 2;
-// A *= 4;
-
-// console.log(A);
-
-// logical operators
-
-// AND operator:
-
-let result;
-let age = 25;
-
-if (age > 20 && age < 30) {
-    result = age + 50;
-}
-else {
-    result = age;
-}
-
-console.log(result);
-
-// OR operator:
-
-let cost = 40;
-let selPrice;
-let profit = 10;
-
-if (cost >= 50 || cost === 55) {
-    selPrice = cost + profit;
-}
-else {
-    selPrice = cost;
-}
-
-console.log(selPrice);
+// console.log(selPrice);
  
 
-// NOT operator
-let name = "Hayzed";
+// // NOT operator
+// let name = "Hayzed";
 
-if (name !== "Hayzed") {
-    console.log("Why are you lying?");
+// if (name !== "Hayzed") {
+//     console.log("Why are you lying?");
+// }
+// else {
+//     console.log("That is good");
+// }
+
+// // loop 
+
+// // while loop
+
+// let num = 5;
+// let i = 0;
+// while (i < num) {
+//     i++;
+//     console.log(i);
+// }
+
+// for (let index = 0; index < num;) {
+//     index++
+//     console.log(index);
+// }
+
+// //  Checking for a leap year
+// // CONDITIONS:
+// // the year divided by 4 should have a remainder of 0
+// // the year  divided by 100 should have a not zero remainder
+// // if year divided by 100 gives no remainder, then divided by 400 should give no remainder
+
+
+// const year = prompt("Enter the year");
+
+// const checkLeap = (year) => {
+//     let result;
+//     if((year % 4 === 0 && year % 100 !== 0) || (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)) {
+//         result = "Yes! it is a leap year";
+//     }
+//     else {
+//         result = "No! it is not a leap year"
+//     }
+
+//     return result;
+// }
+
+// const answer = checkLeap(year);
+
+// alert(answer);
+// // console.log(answer);
+
+// ARRAY AND ITS METHODS
+
+// .forEach()
+
+const names = ["Soliu", "Kenny", "Ola"];
+
+const greeting = names.forEach((name) => {
+    console.log(`${name}, you are welcome`)
+})
+
+// .map()
+
+const numberse = [1, 2, 3, 4]
+
+const numTimesTwo = numberse.map((numb) => {
+    return numb * numberse.indexOf(numb);
+})
+
+console.log(numTimesTwo);
+
+// OBJECT, its methods and declaration types
+
+// Object literal
+
+// constructor function:
+
+function Students (age, name, height) {
+
+    // const this = {}
+    this.age =  age;
+    this.name = name;
+    this.height = height;
+
+    return this;
 }
-else {
-    console.log("That is good");
+
+Students.prototype.isCool = function() {
+    console.log("it is cool")
 }
 
-// loop 
+const student1 = new Students(30, "Soliu", 12);
+console.log(student1);
 
-// while loop
+student1.isCool();
 
-let num = 5;
-let i = 0;
-while (i < num) {
-    i++;
-    console.log(i);
-}
-
-for (let index = 0; index < num;) {
-    index++
-    console.log(index);
-}
-
-//  Checking for a leap year
-// CONDITIONS:
-// the year divided by 4 should have a remainder of 0
-// the year  divided by 100 should have a not zero remainder
-// if year divided by 100 gives no remainder, then divided by 400 should give no remainder
+const student2 = new Students(10, "kola", 24)
+console.log(student2);
 
 
-const year = prompt("Enter the year");
-
-const checkLeap = (year) => {
-    let result;
-    if((year % 4 === 0 && year % 100 !== 0) || (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)) {
-        result = "Yes! it is a leap year";
-    }
-    else {
-        result = "No! it is not a leap year"
-    }
-
-    return result;
-}
-
-const answer = checkLeap(year);
-
-alert(answer);
-// console.log(answer);
 
 
 
